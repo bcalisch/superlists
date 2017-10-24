059e521853d8c1f5ac74585898cff9f51468e806
Benjamin Calisch <bcalisch@gmail.com>
2017-10-23 20:17:30 -0500

    rename all item input ids and names.  still broken

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -135,3 +135,9 @@ class NewListTest(TestCase):
         self.assertEqual(List.objects.count(),0)
         self.assertEqual(Item.objects.count(), 0)
 
+    def test_validation_errors_are_sent_back_to_home_page_template(self):
+        response = self.client.post('/lists/new', data={'text': ''})
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'home.html')
+        expected_error = escape("You can't have an empty list item")
+#        self.assertContains(response, expected_error)
